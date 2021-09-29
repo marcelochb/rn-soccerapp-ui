3b028ea80db8387e8fc0c493a741621736bb3887
Marcelo Block <marcelochb@gmail.com>
2021-09-29 05:37:43 -0300

    fix list properts types

--- src/components/molecules/exhibitions/List/Models/index.d.ts
+++ src/components/molecules/exhibitions/List/Models/index.d.ts
@@ -1,13 +1,13 @@
 import { ModelOfTheme } from "@soccerapp/theme";
 import { Component } from "react";
-import { StyleProp, ViewStyle } from "react-native";
+import { ListRenderItem, StyleProp, ViewStyle } from "react-native";
 
 export type ModelOfList<ItemT = any> = {
   horizontal?: boolean;
   data?: Array<ItemT>;
-  item: (param:ItemT) => React.ReactElement | null;
+  item: ListRenderItem<ItemT> | null | undefined;
   style?: StyleProp<ViewStyle>;
   theme: ModelOfTheme
 }
 
-export class List extends Component<ModelOfList> {}
+export class List<ItemT = any> extends Component<ModelOfList<ItemT>> {}
