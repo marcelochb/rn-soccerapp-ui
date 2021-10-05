d6801365a3305037cc79824b428583811b86e539
Marcelo Block <marcelochb@gmail.com>
2021-10-05 07:56:36 -0300

    CardPlayer export

--- src/components/molecules/interactives/CardPlayer/Models/index.d.ts
+++ src/components/molecules/interactives/CardPlayer/Models/index.d.ts
@@ -1,4 +1,5 @@
 import { ModelOfTheme } from "@soccerapp/theme";
+import { Component } from "react";
 
 export type ModelOfCardPlayer = {
   avatarSource: string | undefined;
@@ -7,3 +8,5 @@ export type ModelOfCardPlayer = {
   navigateTo: (() => void) | undefined;
   theme: ModelOfTheme;
 }
+
+export class CardPlayer extends Component<ModelOfCardPlayer> {}
