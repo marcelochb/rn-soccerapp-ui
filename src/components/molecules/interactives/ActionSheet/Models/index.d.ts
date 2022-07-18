a7dc25b6e62a3d87c30087222b967d744827d1cd
Marcelo Block Teixeira <marcelochb@gmail.com>
2022-07-17 22:54:43 -0300

    export ActionSheet

--- src/components/molecules/interactives/ActionSheet/Models/index.d.ts
+++ src/components/molecules/interactives/ActionSheet/Models/index.d.ts
@@ -1,5 +1,8 @@
 import { ModelOfTheme } from "@soccerapp/theme";
+import { Component } from "react";
 
 export interface ModelOfActionSheet {
   theme: ModelOfTheme;
 }
+
+export class ActionSheet extends Component<ModelOfActionSheet> {}
